700a8ba1c62d3307ab89e0110759ae7a0396b59d
POLinA1234AAA <112808324+POLinA1234AAA@users.noreply.github.com>
2024-02-12 23:21:56 +0000

    some changrs due to the install

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import toast from 'react-hot-toast';
-import { loginUser } from '../../features/LoginSlice';
+import { loginUser} from "../features/LoginSlice"
 
 const Login = () => {
     const dispatch = useDispatch();
